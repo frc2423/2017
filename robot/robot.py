fcad552cc1ab090cc38502439aa07f39ae5b8b5c
David Danielian <David Danielian>
2017-02-25 12:49:50 -0500

    UI pretty much finished, needs to be combined with the robot code

--- robot/robot.py
+++ robot/robot.py
@@ -12,13 +12,18 @@ from networktables.util import ntproperty
 class MyRobot(wpilib.IterativeRobot):
     
     climberspeed = ntproperty("/SmartDashboard/climberspeed", 0)
+    align = ntproperty("/SmartDashboard/align", 0)
+
+    kNoMode = 0
+    kAutonomousMode = 1
+    kTeleopMode = 2
 
     def robotInit(self):
         """
         This function is called upon program startup and
         should be used for any initialization code.
         """
-		
+        self.counter = 0
         wpilib.CameraServer.launch('vision.py:main')
         self.sd = NetworkTables.getTable("SmartDashboard")
         self.motorFL = CANTalon(4)
@@ -29,33 +34,72 @@ class MyRobot(wpilib.IterativeRobot):
         self.joystick1 = wpilib.Joystick(1)
         joystick = wpilib.Joystick(2)
         self.gyro = wpilib.AnalogGyro(0)
+        self.angle_current = self.gyro.getAngle() % 360
+        self.angle = ntproperty("/SmartDashboard/angle", self.angle_current)
+        self.switch = ntproperty("/SmartDashboard/switch", self.kNoMode)
        # self.pid = wpilib.PIDController(0.1, 0.1, 0.1, self.gyro.getAngle(), self.robotdrive)
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
-        self.counter = 0
-
+        self.switch = ntproperty("/SmartDashboard/switch", self.kAutonomousMode)
 
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
+
         self.counter += 1
         if self.counter == 100:
-            print(self.climberspeed)
+            print("Climber Speed: %s" % self.climberspeed)
+            print("Alignment: %s" % self.align)
             self.counter = 0
 
+    def teleopInit(self):
+        self.switch = ntproperty("/SmartDashboard/switch", self.kTeleopMode)
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        rotation = 0
-        
+
+
+
+        self.rotation = 0
+
+        self.counter += 1
+        if self.counter == 100:
+            print("Climber Speed: %s" % self.climberspeed)
+            print("Alignment: %s" % self.align)
+            print("Rotation: %s" % rotation)
+            print("Angle: %s" % self.angle)
+            self.counter = 0
+
         if self.joystick1.getRawButton(4) is True:
             rotation = -.5
         elif self.joystick1.getRawButton(5) is True:
             rotation = .5
         else:
             rotation = 0
-            
-        
+
+        angle_needed = 0
+
+        if self.align == 1:
+            angle_needed = 180
+        elif self.align == 2 or 4:
+            angle_needed = 0
+        elif self.align == 3:
+            angle_needed = -90
+
+
+        def align(self):
+            if self.angle_current > angle_needed:
+                rotation = -.3
+            elif self.angle_current < angle_needed:
+                rotation = .3
+            else:
+                rotation = 0
+
+        if self.align != 0:
+            align(self)
+
+
+
         self.robotdrive.mecanumDrive_Cartesian( 
             self.joystick1.getX(), 
             self.joystick1.getY(), 
@@ -63,7 +107,7 @@ class MyRobot(wpilib.IterativeRobot):
             self.gyro.getAngle()
             )
         
-        
+		
         
     def testPeriodic(self):
         """This function is called periodically during test mode."""
--- ui/www/ui.js
+++ ui/www/ui.js
@@ -13,34 +13,70 @@ $('[name=climb]').on('click', function() {
 });
 
 $('[name=ropel]').on('click', function() {
+	NetworkTables.putValue("/SmartDashboard/align", 1)
 	$(this).addClass('pressed');
+	$('.airship_aligning').css('display', 'block')
 	var $el = $(this);
 	setTimeout(function() {
 		$el.removeClass('pressed');
-	}, 500);
+		NetworkTables.putValue("/SmartDashboard/align", 0);
+		$('.airship_aligning').css('display', 'none')
+	}, 3000);
 })
 
 $('[name=roper]').on('click', function() {
+	NetworkTables.putValue("/SmartDashboard/align", 2)
 	$(this).addClass('pressed');
+	$('.airship_aligning').css('display', 'block')
 	var $el = $(this);
 	setTimeout(function() {
 		$el.removeClass('pressed');
-	}, 500);
+		NetworkTables.putValue("/SmartDashboard/align", 0);
+		$('.airship_aligning').css('display', 'none')
+	}, 3000);
 })
 
 $('[name=ropet]').on('click', function() {
+	NetworkTables.putValue("/SmartDashboard/align", 3)
 	$(this).addClass('pressed');
+	$('.airship_aligning').css('display', 'block')
 	var $el = $(this);
 	setTimeout(function() {
 		$el.removeClass('pressed');
-	}, 500);
+		NetworkTables.putValue("/SmartDashboard/align", 0);
+		$('.airship_aligning').css('display', 'none')
+	}, 3000);
 })
 
 $('[name=ropeb]').on('click', function() {
+	NetworkTables.putValue("/SmartDashboard/align", 4)
 	$(this).addClass('pressed');
+	$('.airship_aligning').css('display', 'block')
 	var $el = $(this);
 	setTimeout(function() {
 		$el.removeClass('pressed');
-	}, 500);
+		NetworkTables.putValue("/SmartDashboard/align", 0);
+		$('.airship_aligning').css('display', 'none')
+	}, 3000);
 })
 
+
+NetworkTables.addKeyListener("/SmartDashboard/angle", function(key, value, isNew){
+	if (value == 500) {	
+		$('#angle').text("?");
+	} else {
+		$('#angle').text(value);
+	}
+}, true);
+
+NetworkTables.addKeyListener("/SmartDashboard/switch", function(key, value, isNew){
+    // do something with the values as they change
+	if (value == 1) {
+		$('#auto_chooser').css('display', 'block');
+		$('#sensors').css('display', 'none')
+	}
+	else if (value == 2) {
+		$('#auto_chooser').css('display', 'none');
+		$('#sensors').css('display', 'block')
+	}
+}, true);
